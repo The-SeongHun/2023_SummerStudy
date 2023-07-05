b29071ad1364e2269a24524cc67a2188ca3cafc4
h0oo0n <eoeou1227@naver.com>
2023-07-05 15:03:33 +0000

    230705 update

--- 4. Arrangement/BOJ_039_5597.cpp
+++ 4. Arrangement/BOJ_039_5597.cpp
@@ -2,19 +2,25 @@
 //나머지
 
 int main(){
-	int n, x, max;
-	x = 0;
+	int n, val, result;
+	result = 0;
+	val = 0;
 	int a[10] = {0};
+	int remain[10] = {0};
 	
-	for (int i = 0; i < 9; i++){
+	for (int i = 0; i <= 9; i++){
+		val = 0;
 		scanf("%d", &a[i]);
-		if (a[i] > max) {
-			max = a[i];
-			x++;
+		remain[i] = a[i] % 42;
+		for (int j = 0; j < i; j++){
+
+			if (remain[j] == remain[i]) val += 1;
+		}
+		if (val == 0){
+			result++;
 		}
 	}
-	
-	printf("%d\n%d", max, x);
+	printf("%d\n", result);
 	
 	return 0;
 }
