27594562f45a3d442f1b94a75a8dd629b1f62350
h0oo0n <eoeou1227@naver.com>
2023-07-23 18:03:10 +0000

    230723 update

---
+++ 8. General Mathematics/BOJ_067_2745.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+//#include <stdbool.h>
+// #include <stdlib.h> //abs, labs
+// #include <math.h> //abs, labs, fabs
+// 진법 변환
+// 
+
+int main() {
+	char num[30]; // 2진법으로 1억이 28자리임
+	int b, result;
+	
+	scanf ("%s %d", num, &b);
+	
+	if (num[0] >= 65) result = num[0] - 55;
+	else result = num[0] - 48;
+	// printf("%d\n", result);
+	
+	int len = strlen(num);
+	
+	for (int i = 1; i < len; i++) {
+		if (num[i] >= 65) num[i] -= 55;
+		else num[i] -= 48;
+		
+		result = (result * b) + num[i];
+		// printf("%d\n", result);
+	}
+	
+	printf("%d\n", result);
+	return 0;
+}
+
